c35d4c50a41da987db6724e7602a52768215abf5
tsabunkar <tsabunkar@gmail.com>
2020-11-25 21:14:33 +0530

    format

--- basics-of-js/pure.js
+++ basics-of-js/pure.js
@@ -1,41 +1,35 @@
-let booksResponse = [{
-        name: 'Infero',
-        genere: 'mystery',
-        author: 'Dan brown'
-    },
-    {
-        name: 'The Notebook',
-        genere: 'mystery',
-        author: 'Spark'
-    },
-    {
-        name: 'Digital fortress',
-        genere: 'scifi',
-        author: 'Dan brown'
-    },
-    {
-        name: 'alchemist',
-        genere: 'journey',
-        author: 'suchi'
-    },
-]
+let booksResponse = [
+  {
+    name: 'Infero',
+    genere: 'mystery',
+    author: 'Dan brown',
+  },
+  {
+    name: 'The Notebook',
+    genere: 'mystery',
+    author: 'Spark',
+  },
+  {
+    name: 'Digital fortress',
+    genere: 'scifi',
+    author: 'Dan brown',
+  },
+  {
+    name: 'alchemist',
+    genere: 'journey',
+    author: 'suchi',
+  },
+];
 
 function modifiyBooks(array) {
-     array.slice(0, 2);
+  array.slice(0, 2);
 }
 
 // console.log(modifiyBooks(booksResponse));
- modifiyBooks(booksResponse)
+modifiyBooks(booksResponse);
 
 console.log('---------');
 
 console.log(booksResponse);
-
-
-
-
-
-
-
 
 // slice() method returns a shallow copy of a portion of an array into a new array object
